94718b70feb7ecdc355782021423cbc52c363f99
Jens Ådne Rydland <jensadne@pvv.ntnu.no>
2013-05-18 14:02:26 +0200

    test for was_published_recently with future poll

--- polls/tests.py
+++ polls/tests.py
@@ -1,16 +1,12 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+import datetime
 
 from django.test import TestCase
+from django.utils import timezone
+
+from polls.models import Poll
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class PollMethodTests(TestCase):
+    def test_was_published_recently_with_future_poll(self):
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
